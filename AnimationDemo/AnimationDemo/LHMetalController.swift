6c5264b4dc36b74ccddd9a75ee0bcf238d104e24
DoN1cK <free.don.ink@gmail.com>
2015-09-10 17:38:27 +0300

    added new initializations

--- AnimationDemo/AnimationDemo/LHMetalController.swift
+++ AnimationDemo/AnimationDemo/LHMetalController.swift
@@ -16,13 +16,17 @@ class LHMetalController: UIViewController {
 	var device = MTLCreateSystemDefaultDevice()
 	var metalLayer = CAMetalLayer()
 	
+	var pipelineState : MTLRenderPipelineState! = nil
+	var commandQueue : MTLCommandQueue! = nil
 	var vertexBuffer : MTLBuffer! = nil
+	
+	var timer :CADisplayLink! = nil
 	
 	// MARK: - UIView life circle
 	
     override func viewDidLoad() {
         super.viewDidLoad()
-
+		setupMetal()
         // Do any additional setup after loading the view.
     }
 
@@ -31,7 +35,26 @@ class LHMetalController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 	
+	
+	// MARK: - timer logic
+	
+	func render() {
+
+	}
+ 
+	func gameloop() {
+		autoreleasepool {
+			self.render()
+		}
+	}
+	
+	
 	// MARK: - Custom methods
+	
+	func setupTimer() {
+		timer = CADisplayLink(target: self, selector: Selector("gameloop"))
+		timer.addToRunLoop(NSRunLoop.mainRunLoop(), forMode: NSDefaultRunLoopMode)
+	}
 	
 	func setupMetal() {
 		
@@ -45,7 +68,32 @@ class LHMetalController: UIViewController {
 					-1.0, -1.0, 0.0,
 					1.0, -1.0, 0.0])
 		//
+		
+		createRenderPipeline()
+		commandQueue = device.newCommandQueue()
+		setupTimer()
 	}
+	
+	func createRenderPipeline() {
+		let defaultLibrary = device.newDefaultLibrary()
+		let fragmentProgram = defaultLibrary!.newFunctionWithName("basic_fragment")
+		let vertexProgram   = defaultLibrary!.newFunctionWithName("basic_vertex")
+		
+		let pipelineStateDescriptor = MTLRenderPipelineDescriptor()
+		pipelineStateDescriptor.vertexFunction = vertexProgram;
+		pipelineStateDescriptor.fragmentFunction = fragmentProgram;
+		pipelineStateDescriptor.colorAttachments[0].pixelFormat = .BGRA8Unorm
+		
+		var pipelineError : NSError?
+		
+		pipelineState = device.newRenderPipelineStateWithDescriptor(pipelineStateDescriptor, error: &pipelineError)
+		
+		if pipelineState == nil {
+			println("Pipeline creation error \(pipelineError)")
+		}
+		
+	}
+	
 	
 	func setVertexData(newVertexData: [Float]) {
 	 let dataSize = newVertexData.count * sizeofValue(newVertexData[0]);
